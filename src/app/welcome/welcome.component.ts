9b305446e984ea0caa31ad57250509a455eb15de
Sven Olding <sven.olding@gmx.de>
2024-02-11 12:37:11 +0100

    convert details component to standalone component

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-welcome',
-  templateUrl: './welcome.component.html'
+  selector: "app-welcome",
+  templateUrl: "./welcome.component.html",
 })
 export class WelcomeComponent {}
